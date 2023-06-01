967540b2691b9e8d422818ed0ffe58dee95293c8
kaser137 <murilon375@gmail.com>
2023-06-01 18:55:23 +0300

    update according to mentor's comments

--- dvmn-bot.py
+++ dvmn-bot.py
@@ -18,18 +18,18 @@ def main():
         try:
             response = requests.get(url, headers=headers, params={'timestamp': f'{timestamp}'}, timeout=91)
             response.raise_for_status()
-            response_entities = response.json()
-            if response_entities['status'] == 'timeout':
-                timestamp = response_entities['timestamp_to_request']
+            review_results = response.json()
+            if review_results['status'] == 'timeout':
+                timestamp = review_results['timestamp_to_request']
             else:
-                timestamp = response_entities['last_attempt_timestamp']
-                if response_entities['new_attempts'][-1]['is_negative']:
+                timestamp = review_results['last_attempt_timestamp']
+                if review_results['new_attempts'][-1]['is_negative']:
                     bot.send_message(chat_id=chat_id,
-                                     text=f'У Вас проверили работу: {response_entities["new_attempts"][-1]["lesson_title"]}'
+                                     text=f'У Вас проверили работу: {review_results["new_attempts"][-1]["lesson_title"]}'
                                           f'\nК сожалению, в работе нашлись ошибки')
                 else:
                     bot.send_message(chat_id=chat_id,
-                                     text=f'У Вас проверили работу: {response_entities["new_attempts"][-1]["lesson_title"]}'
+                                     text=f'У Вас проверили работу: {review_results["new_attempts"][-1]["lesson_title"]}'
                                           f'\nПреподавателю все понравилось, можно приступать к следующему уроку')
         except requests.exceptions.ConnectionError:
             print('connection error, next attempt in 2 seconds')
