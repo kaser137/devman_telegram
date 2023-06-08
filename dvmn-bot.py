7a2e2337b56a86e5cf084a861fd23809b66628ab
kaser137 <murilon375@gmail.com>
2023-06-09 01:34:24 +0300

    add logging to telegram

--- dvmn-bot.py
+++ dvmn-bot.py
@@ -6,6 +6,8 @@ import telegram
 import logging
 
 
+
+
 def main():
     logging.basicConfig(level=logging.INFO)
     dotenv.load_dotenv()
@@ -13,7 +15,15 @@ def main():
     bot_token = os.environ['TG_BOT_TOKEN']
     chat_id = os.environ['TC_CHAT_ID']
     bot = telegram.Bot(token=bot_token)
-    logging.info('start bot')
+
+    class LogTgHandler(logging.Handler):
+        def emit(self, record):
+            bot.send_message(chat_id=chat_id,
+                             text=f'{record}')
+    logger = logging.getLogger('notes')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(LogTgHandler())
+    logger.info('start bot')
     url = 'https://dvmn.org/api/long_polling/'
     headers = {'Authorization': f'Token {dvmn_token}'}
     timestamp = time.time()
@@ -30,12 +40,15 @@ def main():
                     bot.send_message(chat_id=chat_id,
                                      text=f'У Вас проверили работу: {review_results["new_attempts"][-1]["lesson_title"]}'
                                           f'\nК сожалению, в работе нашлись ошибки')
+                    logger.info('работа не сдана')
                 else:
                     bot.send_message(chat_id=chat_id,
                                      text=f'У Вас проверили работу: {review_results["new_attempts"][-1]["lesson_title"]}'
                                           f'\nПреподавателю все понравилось, можно приступать к следующему уроку')
+                    logger.info('работа сдана')
         except requests.exceptions.ConnectionError:
             print('connection error, next attempt in 2 seconds')
+            logger.info('connection error, next attempt in 2 seconds')
             time.sleep(2)
         except requests.exceptions.ReadTimeout:
             pass
